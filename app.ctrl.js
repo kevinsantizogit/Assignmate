9c4fe61232c532273cf1eee3cdebe027e58f950c
Kevin <santizok@mcmaster.ca>
2026-02-27 11:22:58 -0500

    Added tasks controller and the tasks page

--- app.ctrl.js
+++ app.ctrl.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/", require("./controllers/home.ctrl"));
+app.use("/tasks", require("./controllers/tasks.ctrl"));
 
 app.listen(PORT, () => {
   console.log(`Running: http://localhost:${PORT}`);
---
+++ controllers/tasks.ctrl.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.use((req, res, next) => {
+  req.TPL.tasksnav = true;
+  next();
+});
+
+router.get("/", (req, res) => {
+  req.TPL.pageTitle = "Tasks";
+  req.TPL.tasks = [
+    { title: "Finish Deliverable", course: "4WP3" },
+    { title: "Configure SQL", course: "4WP3" }
+  ];
+  res.render("tasks", req.TPL);
+});
+
+module.exports = router;
